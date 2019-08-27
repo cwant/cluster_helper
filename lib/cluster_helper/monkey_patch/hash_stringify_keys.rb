ff3982da25a119ac4f4abff00c30189aec25ea89
Chris Want <cjwant@ualberta.ca>
2019-08-27 15:50:36 -0700

    Make handling Ruby versions better.

--- lib/cluster_helper/monkey_patch/hash_stringify_keys.rb
+++ lib/cluster_helper/monkey_patch/hash_stringify_keys.rb
@@ -1,5 +1,7 @@
-class Hash
-  def stringify_keys
-    Hash[map { |k, h| [k.to_s, h] }]
+unless Hash.instance_methods.include?(:stringify_keys)
+  class Hash
+    def stringify_keys
+      Hash[map { |k, h| [k.to_s, h] }]
+    end
   end
 end
