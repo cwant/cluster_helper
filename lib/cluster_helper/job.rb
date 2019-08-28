643ba5c9fc9b85b22581b405f28a05840af791e1
Chris Want <cjwant@ualberta.ca>
2019-08-27 20:17:44 -0700

    Allow searching jobs by account.

--- lib/cluster_helper/job.rb
+++ lib/cluster_helper/job.rb
@@ -8,6 +8,8 @@ class ClusterHelper::Job
   SQUEUE_FIELDS = [
     { code: '%i',
       name: :id }.freeze,
+    { code: '%u',
+      name: :user }.freeze,
     { code: '%a',
       name: :account }.freeze,
     { code: '%j',
@@ -24,12 +26,27 @@ class ClusterHelper::Job
   USER_JOBS_COMMAND = ("squeue -o '" +
                        SQUEUE_FIELDS.map { |f| '%' + f[:code] }.join('|') +
                        "' -h -u %<user>s").freeze
+  ACCOUNT_JOBS_COMMAND = ("squeue -o '" +
+                          SQUEUE_FIELDS.map { |f| '%' + f[:code] }.join('|') +
+                          "' -h -A %<account>s").freeze
   attr_reader :id, :priority, :state, :user, :account, :name
   attr_reader :submit_time, :start_time
 
   class << self
 
-    def where(username: nil, user: nil)
+    def where(options = {})
+      return where_user(username: options[:username]) if options.key?(:username)
+      return where_user(user: options[:user]) if options.key?(:user)
+      if options.key?(:account_name)
+        return where_account(account_name: options[:account_name])
+      end
+      return where_account(account: options[:account]) if options.key?(:account)
+      []
+    end
+
+    private
+
+    def where_user(username: nil, user: nil)
       return [] if username.nil? && user.nil?
       username = user.username if username.nil?
       user ||= ClusterHelper::User.new(username)
@@ -39,20 +56,49 @@ class ClusterHelper::Job
       # TODO: handle errors
       lines = IO.popen(cmd).readlines
 
-      lines.map { |line| line_to_job(line, user, accounts_cache) }
+      lines.map { |line| user_line_to_job(line, user, accounts_cache) }
     end
 
-    private
+    def where_account(account_name: nil, account: nil)
+      return [] if account_name.nil? && account.nil?
+      account_name = account.name if account_name.nil?
+      account ||= ClusterHelper::Account.new(account_name)
+      cmd = format(ACCOUNT_JOBS_COMMAND, account: account_name)
 
-    def line_to_job(line, user, accounts_cache)
+      user_cache = {}
+      # TODO: handle errors
+      lines = IO.popen(cmd).readlines
+
+      lines.map { |line| account_line_to_job(line, account, user_cache) }
+    end
+
+    def user_line_to_job(line, user, accounts_cache)
       arr = line.strip.split('|')
       out = { user: user }
       SQUEUE_FIELDS.each_with_index do |f, i|
+        next if f[:name] == :user
         if f[:name] == :account
           account = accounts_cache[arr[i]] ||
                     ClusterHelper::Account.new(arr[i])
           accounts_cache[arr[i]] ||= account
           out[:account] = account
+        else
+          out[f[:name]] = arr[i]
+        end
+      end
+      new(out)
+    end
+
+    def account_line_to_job(line, account, user_cache)
+      arr = line.strip.split('|')
+      out = { account: account }
+      SQUEUE_FIELDS.each_with_index do |f, i|
+        next if f[:name] == :account
+        if f[:name] == :user
+          user = user_cache[arr[i]] ||
+                 ClusterHelper::User.new(arr[i])
+          user_cache[arr[i]] ||= user
+          out[:user] = user
         else
           out[f[:name]] = arr[i]
         end
