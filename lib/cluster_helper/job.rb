8b0f4ed5a62ef691fa59e0e8a63a00a9482e9e22
Chris Want <cjwant@ualberta.ca>
2019-12-07 08:54:41 -0800

    Refactor and expand statistics.

--- lib/cluster_helper/job.rb
+++ lib/cluster_helper/job.rb
@@ -1,10 +1,12 @@
 require 'time'
 require_relative 'concerns/methods_to_h'
+require_relative 'concerns/time_readable'
 require_relative 'monkey_patch/hash_stringify_keys'
 
 class ClusterHelper::Job
 
   include MethodsToH
+  include TimeReadable
 
   def to_json(options = {})
     to_h.to_json(options)
@@ -57,24 +59,6 @@ class ClusterHelper::Job
       value = value.strftime('%FT%T') if value
     end
     [key, value]
-  end
-
-  def time_readable(total_seconds)
-    return nil unless total_seconds
-
-    int_seconds = total_seconds.to_i
-    left_over = (total_seconds - int_seconds).round(3)
-    seconds = int_seconds % 60
-    seconds += left_over if left_over > 0.0
-    minutes = (int_seconds / 60) % 60
-    hours = (int_seconds / (60 * 60)) % 24
-    days = int_seconds / (60 * 60 * 24)
-
-    out = ''
-    out += "#{days}d " if days > 0
-    out += "#{hours}h " if days > 0 || hours > 0
-    out += "#{minutes}m #{seconds}s"
-    out
   end
 
   def memory_requested_megabytes
--- lib/cluster_helper/job_statistics.rb
+++ lib/cluster_helper/job_statistics.rb
@@ -1,6 +1,18 @@
+require_relative 'concerns/time_readable'
+
 class ClusterHelper::JobStatistics
-  STATS = [:job_count,
-           :state_histogram].freeze
+
+  include TimeReadable
+
+  STATS = {
+    'job_count' => :job_count,
+    'histograms' => {
+      'states' => :state_histogram,
+      'users' => :user_histogram,
+      'accounts' => :account_histogram
+    }.freeze,
+    'waiting_in_queue' => :waiting_in_queue
+  }.freeze
 
   def initialize(jobs, args)
     @jobs = jobs
@@ -17,27 +29,78 @@ class ClusterHelper::JobStatistics
   end
 
   def state_histogram
-    return if @jobs.empty?
-    histogram = {}
-    @jobs.each do |job|
-      histogram[job.state] ||= 0
-      histogram[job.state] += 1
+    generic_histogram('state', &:state)
+  end
+
+  def user_histogram
+    generic_histogram('username') do |job|
+      job.user.username
     end
-    histogram
+  end
+
+  def account_histogram
+    generic_histogram('account') do |job|
+      job.account.name
+    end
   end
 
   def execute
     init_stats
-    STATS.each do |stat|
-      data = send(stat)
-      @stats[stat.to_s] = data if data
+    @stats = execute_node(STATS)
+  end
+
+  def execute_node(node)
+    out = {}
+    node.each do |name, stat|
+      data = if stat.is_a?(Hash)
+               execute_node(stat)
+             else
+               send(stat)
+             end
+      out[name] = data if data
     end
+    return nil if out.empty?
+    out
   end
 
   def to_h
     return @stats if @stats
     execute
     @stats
+  end
+
+  def waiting_in_queue
+    total_waiting_time_seconds = 0
+    total_jobs = 0
+    @jobs.each do |job|
+      wait_time_seconds = job.time_in_queue_seconds
+      if wait_time_seconds
+        total_waiting_time_seconds += wait_time_seconds
+        total_jobs += 1
+      end
+    end
+    return nil if total_jobs == 0
+    average_time_seconds = total_waiting_time_seconds / total_jobs
+    { 'job_count' => total_jobs,
+      'total_time' => time_readable(total_waiting_time_seconds),
+      'mean_time' => time_readable(average_time_seconds) }
+  end
+
+  private
+
+  def generic_histogram(key_name)
+    return if @jobs.empty?
+    histogram = {}
+    @jobs.each do |job|
+      key = yield(job)
+      histogram[key] ||= 0
+      histogram[key] += 1
+    end
+    return nil if histogram.empty?
+    histogram.collect do |key, value|
+      { key_name => key,
+        'job_count' => value }
+    end
   end
 
 end
